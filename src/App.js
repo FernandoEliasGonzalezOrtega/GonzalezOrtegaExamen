3e29a79ba15ffaaa896c39ce18a054f7038379a1
Fernando Elias González Ortega <317014596@pcpuma.acatlan.unam.mx>
2022-05-31 02:28:24 -0500

    310522

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
+  const getPost = async () =>{
+    try {
+      let formData = new FormData(); 
+      
+      //Url del API
+      const urlPost = "https://jsonplaceholder.typicode.com/posts";
+
+      let result = await axios({
+        urlPost,
+        method: 'GET',
+        dataType: 'json',
+        ContentType: 'application/json',
+        data: formData
+        });
+        
+
+        let datos =result.data;
+        console.log(datos);
+        
+        if(datos===0){
+          alert("No hay datos :( ");
+          return(0);
+        }
+
+        
+
+
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container-xl'>
+        <div className='row text-center mt-4'>
+          <h1>Segundo Examen</h1>
+          <div className='col-md-6'>
+            <button className='btn btn-primary' type='button' onClick={getPost}>Primer botón</button>
+          </div>
+          <div className='col-md-6'>
+            <button className='btn btn-secondary' type='button'>Segundo botón</button>
+          </div>
+        </div>
+        <div id='cambio'>
+          <div className='row text-center mt-4'>
+            <div className='col-md-12'>
+              <h1>Posts</h1>
+            </div>
+          </div>
+
+          <div className='table-responsive-md'>
+            <table className='table'>
+              <thead>
+              <tr>
+                <th scope="col">Id del Usuario</th>
+                <th scope="col">#</th>
+                <th scope="col">Titulo</th>
+                <th scope="col">Post</th>
+              </tr>
+              </thead>
+              <tbody>
+              
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
