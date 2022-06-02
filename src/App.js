22404a87f487a1a1a376fb27441b3981c82a12a7
Fernando Elias González Ortega <317014596@pcpuma.acatlan.unam.mx>
2022-06-01 21:35:45 -0500

    Ultima versión

--- src/App.js
+++ src/App.js
@@ -1,89 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import axios from 'axios';
+import Posts from './components/posts';
+import Fotos from './components/fotos';
+
 
 function App() {
-  
-  const getPost = async () =>{
+  const url1 = "https://jsonplaceholder.typicode.com/posts";
+  const url2 = "https://jsonplaceholder.typicode.com/photos";
+  const [json, setJson] = useState();
+  const [table, showTable] = useState(0);
+  const [photo, showPhoto] = useState(0);
+
+  const getAPI = async (urlAux) =>{
     try {     
       //Url del API
-      const url = "https://jsonplaceholder.typicode.com/posts";
-
+      const url = urlAux;
+      let formData = new FormData();
       let result = await axios({
         url,
-        method: 'GET'
+        method: 'GET',
+        dataType: "json",
+        ContentType: "application/json",
+        data: formData
       });
         
-        let datos =result.data;
-        console.log(datos);
-        
-        if(datos===0){
-          alert("No hay datos :( ");
-          return(0);
-        }
+      let datos =result.data;
+      console.log(datos);
+      setJson(datos);
+      return datos;
     }
     catch (error) {
       console.log(error);
     }
   }
 
-    
+  const mostrarTabla = async () => {
+    showPhoto(0);
+    setJson(await getAPI(url1));
+    showTable(1);
+  } 
+  
+  const mostrarFotos = async () => {
+    showTable(0);
+    setJson(await getAPI(url2));
+    showPhoto(1);
+  } 
 
   return (
-    <div className='App'>
+    <div className='App bg1'>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand">Fernando Elias González Ortega-----31701459------Desarrollo Web</a>
+        </div>
+      </nav>
       <div className='container-xl'>
         <div className='row text-center mt-4'>
           <h1>Segundo Examen</h1>
           <div className='col-md-6'>
-            <button className='btn btn-primary' type='button' onClick={getPost}>Primer botón</button>
+            <button className='btn btn-primary' type='button' onClick={mostrarTabla}>Primer botón</button>
           </div>
           <div className='col-md-6'>
-            <button className='btn btn-secondary' type='button'>Segundo botón</button>
+            <button className='btn btn-secondary' type='button' onClick={mostrarFotos}>Segundo botón</button>
           </div>
         </div>
-        <div id='cambio'>
-
-          {/*[API POST]*/}
-          <div className='row text-center mt-4'>
-            <div className='col-md-12'>
-              <h1>Posts</h1>
-            </div>
-          </div>
-
-          <div className='table-responsive'>
-            <table className='table table-bordered'>
-              <thead>
-              <tr>
-                <th scope="col">id</th>
-                <th scope="col">#</th>
-                <th scope="col">Titulo</th>
-                <th scope="col">Post Completo</th>
-              </tr>
-              </thead>
-              <tbody>
-                {/*[Aquí va lo que salga del json]*/}
-              </tbody>
-            </table>
-          </div>
-
-          {/*[API Imagenes]*/}
-          <div className='row text-center mt-4'>
-            <div className='col-md-12'>
-              <h1>Imágenes</h1>
-            </div>
-          </div>
-
-          <div className='row text-center mt-4'>
-            <div className='col-md-3'>
-              
-              <img src=''/>
-
-            </div>
-          </div>
-
+        <div className='bg2'>
+          <Posts show = {table} data = {json} />
+          <Fotos show = {photo} data = {json} />
         </div>
-      </div>
-      
+        </div>
+      <footer class="navbar navbar-dark bg-dark fixed-bottom">
+        <div class="container">
+          <p class="navbar-brand">Fernando Elias González Ortega-----31701459------Desarrollo Web</p>
+        </div>
+      </footer>
     </div>
   );
 }
